8a73d8d4fe23898b2512075fbafb1928ce0420aa
Engkos <engkoskostaman759@gmail.com>
2023-01-14 20:19:06 +0700

    add appbar widget

--- lib/main.dart
+++ lib/main.dart
@@ -7,12 +7,22 @@ void main() {
 class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.red,
+          leading: Icon(Icons.home),
+          title: Center(child: Text("Engkos App")),
+          actions: <Widget>[Icon(Icons.search)],
+        ),
         body: Container(
             color: Colors.green[700],
             width: 400.0,
             height: 200.0,
             child: Center(
-                child: Icon(Icons.android, color: Colors.blue, size: 80.0))
+                child: Icon(
+              Icons.android,
+              color: Colors.blue,
+              size: 80.0,
+            ))
             // child: Center(
             //     child: Text("JANGAN LUPA BELAJAR ANDROID",
             //         style: TextStyle(
