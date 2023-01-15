86105227e42c05d6046a8acfc5102319a122a45b
Engkos <engkoskostaman759@gmail.com>
2023-01-15 22:38:42 +0700

    add column

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,24 +9,40 @@ void main() {
 class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.red,
-          leading: Icon(Icons.home),
-          title: Center(child: Text("Engkos App")),
-          actions: <Widget>[Icon(Icons.search)],
-        ),
-        body: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: <Widget>[
-            Image.network(
-              "https://images.unsplash.com/photo-1569226022944-7c948d846270?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60",
-              height: 220.0,
-            ),
-            Image.network(
-              "https://images.unsplash.com/photo-1569226022944-7c948d846270?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60",
-              height: 220.0,
-            ),
-          ],
-        ));
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        leading: Icon(Icons.home),
+        title: Center(child: Text("Engkos App")),
+        actions: <Widget>[Icon(Icons.search)],
+      ),
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.all(20.0),
+          ),
+          Center(
+              child: Text(
+            "Engkos kostaman",
+            style: TextStyle(color: Colors.red, fontSize: 20.0),
+          )),
+          Padding(
+            padding: EdgeInsets.all(20.0),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              Image.network(
+                "https://images.unsplash.com/photo-1569226022944-7c948d846270?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60",
+                height: 200.0,
+              ),
+              Image.network(
+                "https://images.unsplash.com/photo-1569226022944-7c948d846270?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60",
+                height: 200.0,
+              ),
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
